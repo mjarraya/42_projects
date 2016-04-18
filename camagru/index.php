ecbfa16f4ae0f4a1c86c2015349ba1bdd2a5f472
Montasar JARRAYA <mjarraya@e2r7p17.42.fr>
2016-04-18 21:23:05 +0200

    camagru

---
+++ camagru/index.php
@@ -0,0 +1,98 @@
+<html>
+<head>
+<title>Camagru</title>
+<style>
+header {
+	height:10%;
+	background-color: pink;
+	text-align: center;
+	font-family: tahoma;
+}
+footer{
+	background-color: lightblue;
+	height:8%;
+}
+#cover{
+	position:absolute;
+	margin-left: 50%;
+	margin-right: 50%;
+	z-index: 1;
+	width: 560px;
+	height: 420px;
+	background-image: url("http://vignette4.wikia.nocookie.net/robber-penguin-agency/images/6/6e/Small-mario.png");
+	background-repeat: no-repeat;
+}
+</style>
+</head>
+<body>
+	<header><h1>Camagru</h1></header>
+	<center><video id="video"></video><br/>
+	<button id="startbutton">Prendre une photo</button><br/>
+	<div id="cover"></div>
+	<canvas id="canvas"></canvas></center>
+	<img id="photo">
+<script>
+(function(){
+
+	var streaming = false,
+		video = document.querySelector('#video'),
+		cover = document.querySelector('#cover'),
+		canvas = document.querySelector('#canvas'),
+		photo = document.querySelector('#photo'),
+		startbutton = document.querySelector('#startbutton'),
+		width = 560,
+		height = 0;
+
+	navigator.getMedia = (navigator.getUserMedia ||
+		navigator.webkitGetUserMedia ||
+		navigator.mozGetUserMedia ||
+		navigator.msGetUserMedia);
+
+	navigator.getMedia(
+		{
+			video: true,
+				audio: false
+		},
+		function(stream){
+			if (navigator.mozGetUserMedia){
+				video.mozSrcObject = stream;
+			}
+			else{
+				var vendorURL = window.URL || window.webkitURL;
+				video.src = vendorURL.createObjectURL(stream);
+			}
+			video.play();
+		},
+			function(err){
+				console.log("An error occured! " + err);
+			}
+	);
+	video.addEventListener('canplay', function(ev){
+		if (!streaming) {
+			height = video.videoHeight / (video.videoWidth/width);
+			video.setAttribute('width', width);
+			video.setAttribute('height', height);
+			canvas.setAttribute('width', width);
+			canvas.setAttribute('height', height);
+			console.log(height);
+			streaming = true;
+		}
+	}, false);
+	function takepicture(){
+		canvas.width = width;
+		canvas.height = height;
+		canvas.getContext('2d').drawImage(video, 0, 0, width, height);
+		var data = canvas.toDataURL('image/png');
+		canvas.setAttribute('src', data);
+	}
+
+	startbutton.addEventListener('click', function(ev){
+		takepicture();
+		ev.preventDefault();
+	}, false);
+})();
+</script>
+</body>
+<footer>
+</footer>
+</html>
